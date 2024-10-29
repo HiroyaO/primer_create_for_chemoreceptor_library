a595101d324bf8c2d5eff9be5189b93ec9b06f3d
HiroyaO <160078401+HiroyaO@users.noreply.github.com>
2024-10-29 11:31:22 -0700

    Update primer_creater.py

--- primer_creater.py
+++ primer_creater.py
@@ -16,7 +16,7 @@ import primer3
 file_path = "input_genes.csv" # the table that include target genes
 gene_column = "GeneSequence"  # the name of column of gene sequences
 
-df = pd.read_csv(file_path)
+df = pd.read_csv(file_path) # you need to change if needed. Ex, elsx.
 
 # Define the specific sequences to prepend to each primer
 
@@ -32,14 +32,14 @@ def design_primers(sequence):
     
     # Primer design conditions
     primer_conditions = {
-        'PRIMER_OPT_SIZE': 20,
-        'PRIMER_MIN_SIZE': 18,
-        'PRIMER_MAX_SIZE': 25,
-        'PRIMER_OPT_TM': 60.0,
-        'PRIMER_MIN_TM': 57.0,
-        'PRIMER_MAX_TM': 63.0,
-        'PRIMER_MIN_GC': 40,
-        'PRIMER_MAX_GC': 60,
+        'PRIMER_OPT_SIZE': 25,
+        'PRIMER_MIN_SIZE': 17,
+        'PRIMER_MAX_SIZE': 36,
+        'PRIMER_OPT_TM': 63.0,
+        'PRIMER_MIN_TM': 55.0,
+        'PRIMER_MAX_TM': 66.0,
+        'PRIMER_MIN_GC': 30,
+        'PRIMER_MAX_GC': 70,
         'PRIMER_PRODUCT_SIZE_RANGE': [[sequence_length, sequence_length]],  # Amplify the entire sequence
     }
     
@@ -52,11 +52,16 @@ def design_primers(sequence):
         primer_conditions
     )
     
-    # Get the primer results
-    forward_primer = fwd_prefix + primer_results['PRIMER_LEFT_0_SEQUENCE']
-    reverse_primer = rev_prefix + primer_results['PRIMER_RIGHT_0_SEQUENCE']
-    forward_tm = primer_results['PRIMER_LEFT_0_TM']
-    reverse_tm = primer_results['PRIMER_RIGHT_0_TM']
+    # Check if primer design was successful
+    try:
+        forward_primer = fwd_prefix + primer_results['PRIMER_LEFT_0_SEQUENCE']
+        reverse_primer = rev_prefix + primer_results['PRIMER_RIGHT_0_SEQUENCE']
+        forward_tm = primer_results['PRIMER_LEFT_0_TM']
+        reverse_tm = primer_results['PRIMER_RIGHT_0_TM']
+    except KeyError:
+        # Return empty values if primer design failed
+        forward_primer = reverse_primer = "Design Failed"
+        forward_tm = reverse_tm = None
     
     return forward_primer, reverse_primer, forward_tm, reverse_tm
 
